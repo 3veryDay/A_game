04e51a10a1bce8f3ef4acafa02cecdf265bd86d2
3veryday <hyunseo328@ewhain.net>
2025-03-28 10:26:37 +0900

    Login request, response class created

--- DESIGN/security/SecurityDemo/src/main/java/com/example/securitydemo/GreetingsController.java
+++ DESIGN/security/SecurityDemo/src/main/java/com/example/securitydemo/GreetingsController.java
@@ -1,34 +1,87 @@
 package com.example.securitydemo;
 
+import jwt.JwtUtils;
+import jwt.LoginRequest;
+import jwt.LoginResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.security.authorization.AuthorityReactiveAuthorizationManager.hasRole;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import static org.springframework.security.authorization.AuthorityReactiveAuthorizationManager.hasRole;
+import org.springframework.security.core.Authentication;
+import java.util.HashMap;
+import java.util.Map;
 @RestController
 @EnableWebSecurity
 //그래야지 preAuthorize를 사용할 수 있음
 @EnableMethodSecurity
 public class GreetingsController {
 
+    private AuthenticationManager authenticationManager;
+
+    private JwtUtils jwtUtils;
+
+
+
     @GetMapping("/hello")
     public static String main(String[] args) {
         return "Hello, World!";
     }
-//check user credentials before acting on method
+
+    //check user credentials before acting on method
     @PreAuthorize("hasRole('ADMIN')or hasRole('USER')")
     @GetMapping("/user")
-    public String userEndPoint(){
+    public String userEndPoint() {
         return "Hello User!";
     }
 
 
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
-    public String adminEndPoint(){
+    public String adminEndPoint() {
         return "Hello admin!";
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication;
+        try {
+            authentication = authenticationManager.authenticate
+                    (new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+        } catch (AuthenticationException exception) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("message", "Bad credentials");
+            map.put("status", false);
+            return new ResponseEntity<Object>(map, HttpStatus.NOT_FOUND);
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        String jwtToken = jwtUtils.generateTokenFromUsername(userDetails);
+
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        LoginResponse response = new LoginResponse(userDetails.getUsername(), jwtToken, roles);
+
+        return ResponseEntity.ok(response);
     }
 }
